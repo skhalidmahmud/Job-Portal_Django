9394c807097ca667acba44039aac1db32c893f45
Khalid Mahmud <skhalidmahmud1@gmail.com>
2025-07-21 04:12:34 +0600

    Update profile for employer
    After login, employer can update there employerprofile from employerProfileModel
    
    Co-Authored-By: Humaira Akter <213163679+humairaaktershanti@users.noreply.github.com>

--- job_portal_project/employer_app/views.py
+++ job_portal_project/employer_app/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
-# Create your views here.
+
 def updateProfiles(req):
-    return render(req, 'updateProfiles.html')
+    data = employerProfileModel.objects.filter(employerUser=req.user).first()
+    context = {
+        'data': data
+    }
+    if not data:
+        data = employerProfileModel.objects.create(employerUser=req.user)
+
+    if req.method=='POST':
+        data.companyName=req.POST.get('companyName')
+        data.aboutConpany=req.POST.get('aboutConpany')
+        if req.FILES.get('conpanyLogo'):
+            data.conpanyLogo=req.FILES.get('conpanyLogo')
+        data.location=req.POST.get('location')
+
+        data.save()
+
+        return redirect('index')
+    
+    return render(req, 'updateProfiles.html', context)
