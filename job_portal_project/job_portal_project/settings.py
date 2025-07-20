8fa84d53576ebfc49d3bec48b00b04eccb88bfdb
Khalid Mahmud <94211717+skhalidmahmud@users.noreply.github.com>
2025-07-20 12:33:06 +0600

    Make Custom user model and migration migrate it
    Make two model : customUserModel and pendingAccountModel
    makemigrations and migrate
    ```bash
    py manage.py makemigrations users_auth_app
    py manage.py migrate users_auth_app
    py manage.py migrate
    ```
    
    and create two superuser:
    username : admin, khalid
    password :  admin, 1234

--- job_portal_project/job_portal_project/settings.py
+++ job_portal_project/job_portal_project/settings.py
@@ -109,3 +109,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL="users_auth_app.customUserModel"
+
+STATICFILES_DIRS=[
+    BASE_DIR / "static",
+]
--- job_portal_project/users_auth_app/models.py
+++ job_portal_project/users_auth_app/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class customUserModel(AbstractUser):
+    USERTYPE = [
+        ('Admin','Admin'),
+        ('Employer','Employer'),
+        ('Candidate','Candidate')
+    ]
+    phone = models.CharField(max_length = 100, null = True)
+    userTypes = models.CharField(choices = USERTYPE, max_length = 100, null = True)
+
+class pendingAccountModel(models.Model):
+    username = models.CharField(max_length = 100, null = True)
+    email = models.EmailField(null = True)
+    phone = models.CharField(max_length = 100, null = True)
+    USERTYPE = [
+        ('Employer','Employer'),
+        ('Candidate','Candidate')
+    ]
+    userTypes = models.CharField(choices = USERTYPE, max_length = 100, null = True)
+    STATUS = [
+        ('Pending','Pending'),
+        ('Accept','Accept'),
+        ('Rejected','Rejected')
+    ]
+    pendingStatus = models.CharField(choices = STATUS, max_length = 100, null = True)
